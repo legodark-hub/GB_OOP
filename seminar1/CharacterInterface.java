ac0280fc3e6e5d866629086baea1fa31a9ebed96
aidarminyazev <legodark@mail.ru>
2023-07-07 23:07:55 +0500

    step method in CharacterInterface updated

--- seminar1/CharacterInterface.java
+++ seminar1/CharacterInterface.java
@@ -1,6 +1,8 @@
 package GB_OOP.seminar1;
 
+import java.util.ArrayList;
+
 public interface CharacterInterface {
-    void step();
+    void step(ArrayList<Character> enemies, ArrayList<Character> allies);
     String getInfo();
 }
