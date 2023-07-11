ef206bb08686da5400e979c0042fdeffb9a8f9df
aidarminyazev <legodark@mail.ru>
2023-07-11 19:26:02 +0500

    peasant busy state added

--- seminar1/Classes/Peasant.java
+++ seminar1/Classes/Peasant.java
@@ -3,8 +3,11 @@ package GB_OOP.seminar1.Classes;
 import java.util.ArrayList;
 
 public class Peasant extends Character{
+    private boolean busy;
+
     public Peasant(String name, int x, int y) {
         super(name, 100, 3, 1, x, y);
+        this.busy = false;
     }
 
     @Override
@@ -15,6 +18,7 @@ public class Peasant extends Character{
 
     @Override
     public void step(ArrayList<Character> enemies, ArrayList<Character> allies) {
+        busy = false;
         //attack();
     }
 }
